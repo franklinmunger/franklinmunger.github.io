bd7b1da0fd1a2f4ce1122d822987aef4f4f7fffa
Franklin Munger <franklin.munger@gmail.com>
2020-09-12 20:32:33 +0000

    added javascripto functionality to toggle profile picture image and css styles for the border

--- main.js
+++ main.js
@@ -6,5 +6,17 @@ $(document).ready(function(){
         $('html, body').animate({scrollTop: $('#projects').offset().top},
             1000);
     });
+    
+    $( "#profile-picture" ).click(function() {
+        
+        const originalImage = "https://i.imgur.com/rBkFyvz.jpg";
+        
+        const secondImage = "https://westernmustangs.ca/images/2014/10/7//Ademiden_Toniwa_cropped.jpg?width=300";
+        
+        const currentImage =  $("#profile-picture").attr("src");
+        
+        
+        $("#profile-picture").attr("src", currentImage === originalImage ? secondImage : originalImage);
+    });
 
 });
