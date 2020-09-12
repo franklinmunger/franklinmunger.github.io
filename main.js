b62b58aff7c1c1051f63729db72ee6a27392c95c
Franklin Munger <franklin.munger@gmail.com>
2020-09-12 18:05:27 +0000

    Revert "added id to profile picture, border style to styles.css and toggle profile picture image on each click in main.js"
    This reverts commit df65f1a90251c2306867ea756b7b7e91b00d3269.

--- main.js
+++ main.js
@@ -6,14 +6,5 @@ $(document).ready(function(){
         $('html, body').animate({scrollTop: $('#projects').offset().top},
             1000);
     });
-    
-    $("#profile-picture").click(function(){
-        // Change src attribute of image
-        const image1 = "https://i.imgur.com/ZiaWCXz.jpg";
-        
-        const image2 = "https://westernmustangs.ca/images/2014/10/7//Ademiden_Toniwa_cropped.jpg?width=300";
-        
-        $(this).attr("src", $(this).attr("src") === image1 ? image2 : image1);
-    });
 
 });
